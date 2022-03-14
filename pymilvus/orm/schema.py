be6071933669f661db9b30ae7eb367d10a19b770
dragondriver <jiquan.long@zilliz.com>
2022-03-14 14:53:51 +0800

    Support VarChar data type
    Signed-off-by: dragondriver <jiquan.long@zilliz.com>

--- pymilvus/orm/schema.py
+++ pymilvus/orm/schema.py
@@ -201,7 +201,7 @@ class FieldSchema:
 
     def _parse_type_params(self):
         # update self._type_params according to self._kwargs
-        if self._dtype not in (DataType.BINARY_VECTOR, DataType.FLOAT_VECTOR):
+        if self._dtype not in (DataType.BINARY_VECTOR, DataType.FLOAT_VECTOR, DataType.STRING, DataType.VARCHAR,):
             return
         if not self._kwargs:
             return
